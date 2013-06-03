834cfa260170331120ae5751c96201f5399230fe
Dillon Hafer <dh@dillonhafer.com>
2013-06-03 19:10:40 -0400

    Updated new deploy script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,51 @@
-require 'bundler/capistrano'
+require "bundler/capistrano"
 
-set :application, "fpu"
+server "192.34.63.57", :web, :app, :db, primary: true
+
+set :application, "budgets"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:thealtair/budgets.git"
+set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-role :app, "50.116.23.78"
-role :web, "50.116.23.78"
-role :db, "50.116.23.78", :primary => true
-
-set :deploy_to, "/var/www/intense-money.com"
-set :deploy_via, :remote_cache
-set :use_sudo, false
-
-set :scm, :git
-set :repository, "git@dev.dillonhafer.com:/var/git/intense-money.com.git"
-set :branch, "master"
-
-set :user, "fpu"
-
-after "deploy:update_code" do
-  #run "ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
-  #run "ln -s #{shared_path}/1001303134.pem #{release_path}/config/1001303134.pem"
-  #run "ln -s #{shared_path}/videos #{release_path}/public/videos"
-  run "cd #{release_path} ; RAILS_ENV=production bundle exec rake assets:precompile --trace"
-  # Fix group so cached javascripts and stylesheets can be generated
-  #run "chgrp FBCS #{release_path}/public/javascripts"
-  #run "chgrp FBCS #{release_path}/public/stylesheets"
-end
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
-  task :start, :roles => :app do
-    restart
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
   end
-  
-  task :restart, :roles => :app do
-    run "touch #{File.join(current_path, "tmp", "restart.txt")}"
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
   end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
 end
