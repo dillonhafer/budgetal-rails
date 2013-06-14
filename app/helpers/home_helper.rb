eb32f44697b7a23bbfa6c5f6bdc6713ee005ca48
Dillon Hafer <dh@dillonhafer.com>
2013-06-14 14:16:53 -0400

    Added dynamic colors

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,3 +1,4 @@
+
 module HomeHelper
   def find_short_month(n)
     Date.new( 1, n).strftime("%b")
@@ -5,5 +6,15 @@ module HomeHelper
 
   def is_envelope?(budget_item)
     return "<i class='general foundicon-mail blue'></i>".html_safe if budget_item.envelope
+  end
+
+  def remaining_to_budget
+    if @budget_remaining > 0.00
+      "#{number_to_currency @budget_remaining} Remaining to budget"
+    elsif @budget_remaining == 0
+      "<span style='color:#69F'>Congratulations! Your budget is balanced!</span>".html_safe
+    else
+      "<span style='color:red'>#{number_to_currency @budget_remaining.abs} Over budget!</span>".html_safe
+    end
   end
 end
