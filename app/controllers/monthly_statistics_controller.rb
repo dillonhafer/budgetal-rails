5de9270fbb6cb3cc6443be8a23338856dc6e1aef
Dillon Hafer <dh@dillonhafer.com>
2013-06-18 19:25:36 -0400

    Added charts.

--- app/controllers/monthly_statistics_controller.rb
+++ app/controllers/monthly_statistics_controller.rb
@@ -6,6 +6,6 @@ class MonthlyStatisticsController < ApplicationController
 
   def show
     @date = Date.new(params[:year].to_i, params[:month].to_i).strftime("%B %Y")
-    @budget = Budget.find_by_month(params[:month])
+    @budget = Budget.find_by_month_and_user_id(params[:month], current_user.id)
   end
 end
--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,6 +1,7 @@
 class Budget < ActiveRecord::Base
   attr_accessible :monthly_income  
   attr_accessor :year
+
   belongs_to :user
   has_many :budget_categories
   has_many :budget_items, through: :budget_categories
