393a7d9a95c33bb038dd7e30ad30a1f6c75602e0
Dillon Hafer <dh@dillonhafer.com>
2013-07-23 23:31:21 -0400

    Lowered number of instances for auto-fill needed.

--- app/models/budget_item.rb
+++ app/models/budget_item.rb
@@ -8,15 +8,13 @@ class BudgetItem < ActiveRecord::Base
     sql = <<-END_SQL
         SELECT budget_item_expenses.name
         FROM budgets
-        JOIN budget_categories on budget_categories.budget_id=budgets.id
-        JOIN budget_items on budget_items.budget_category_id=budget_categories.id
-        JOIN budget_item_expenses on budget_item_expenses.budget_item_id=budget_items.id
+        JOIN budget_categories ON budget_categories.budget_id=budgets.id
+        JOIN budget_items ON budget_items.budget_category_id=budget_categories.id
+        JOIN budget_item_expenses ON budget_item_expenses.budget_item_id=budget_items.id
         WHERE budget_items.name = ?
-        AND budgets.user_id = #{self.budget_category.budget.user_id}
-        -- Perhaps this is premature optimization
-        -- AND budgets.created_at::date BETWEEN '#{Date.today}' AND '#{6.month.ago.to_date}'
-        GROUP by budget_item_expenses.name
-        HAVING (count(budget_item_expenses.name) > 3)
+        AND budgets.user_id = #{self.budget_category.budget.user_id}        
+        GROUP BY budget_item_expenses.name
+        HAVING (COUNT(budget_item_expenses.name) > 2)
     END_SQL
 
     expenses = BudgetItem.find_by_sql([sql, self.name])
