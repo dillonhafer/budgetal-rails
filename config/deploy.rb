3e03523924d7c3b70e24ed1aa615a8fce84b6bce
Dillon Hafer <dh@dillonhafer.com>
2014-01-07 13:59:13 -0500

    Updates deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:thealtair/budgets.git"
+set :repository, "git@bitbucket.org:dhafer/budgets.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
