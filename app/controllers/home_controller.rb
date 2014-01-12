0efc5d98091fbc5de43e965d4f45993358d1e44a
Dillon Hafer <dh@dillonhafer.com>
2014-01-11 23:27:35 -0500

    Adds before filter for valid date ranges

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   before_filter :require_user  
+  before_filter :check_date  
 
   def index
-    year  = params[:year]  || Date.today.year
-    month = params[:month] || Date.today.month
+    year  = params[:year]
+    month = params[:month]
     
     @budgets = []
     (1..12).each do |month|
@@ -11,5 +12,18 @@ class HomeController < ApplicationController
     end
 
     @budget = current_user.budgets.where(month: month.to_s, year: year.to_s).first || Budget.create_template(month, year, current_user.id)
-  end  
+  end
+
+  def check_date
+    if params[:year].to_i > 2020
+      flash[:error] = "You can't go that far into the future. Who's to say we'd still be around?"
+      redirect_to root_path
+    elsif params[:year].to_i < 2013
+      flash[:error] = "We didn't exist back then, I don't think you'll find a budget there."
+      redirect_to root_path      
+    elsif !(1..12).include?(params[:month].to_i)
+      flash[:error] = "You do know there are only 12 months in a year right?"
+      redirect_to root_path
+    end
+  end
 end
