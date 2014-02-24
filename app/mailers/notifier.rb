beb605b9c5a01d83292ff8602de9c163836a382e
Dillon Hafer <dh@dillonhafer.com>
2014-02-24 11:22:56 -0500

    Updates layouts

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,22 +1,25 @@
 class Notifier < ActionMailer::Base
   def new_user(user)
     @user = user
-    mail(:from => "no-reply@intense-money.com", :to => @user.email, :subject => "Welcome to Intense-Money.com!")
+    mail(:from => "no-reply@budgetal.com", :to => @user.email, :subject => "Welcome to budgetal.com!")
   end
   
   def send_receipt(order)
     @order = order
-    mail(:from => "orders@intense-money.com", :to => @order.email, :subject => "Your reciept for order #: #{@order.order_number}")
+    mail(:from => "orders@budgetal.com", :to => @order.email, :subject => "Your reciept for order #: #{@order.order_number}")
   end
   
   def password_reset(user)
     @user = user
-    mail(:from => "no-reply@intense-money.com", :to => @user.email, :subject => "Password reset")
+    mail(:from => "no-reply@budgetal.com", :to => @user.email, :subject => "Password reset")
   end
   
   def push_notification(to, message)
     @message = message
-    mail(:from => "text-notifications@intense-money.com", :to => to, :subject => "Action Required")
+    mail(:from => "text-notifications@budgetal.com", :to => to, :subject => "Action Required")
   end
-  
+
+  def new_website(user)
+    mail(:from => "Budgetal <no-reply@budgetal.com>", :to => @user.email, :subject => "We're getting a makeover!")
+  end
 end
