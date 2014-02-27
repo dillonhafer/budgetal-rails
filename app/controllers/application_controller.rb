88256416089542de076066dc0754bbe88295364c
Dillon Hafer <dh@dillonhafer.com>
2014-02-27 13:38:52 -0500

    Remove puts from app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,7 @@ class ApplicationController < ActionController::Base
     if request.xhr?
       #avoiding XSS injections via flash
       flash_json = Hash[flash.map{|k,v| [k,ERB::Util.h(v)] }].to_json
-      response.headers['X-Flash-Messages'] = flash_json
-      puts flash
+      response.headers['X-Flash-Messages'] = flash_json      
       flash.discard
     end
   end
