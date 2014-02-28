a5e941335318ce6bb1e9731614e7ab89dd341272
Dillon Hafer <dh@dillonhafer.com>
2014-02-27 21:07:56 -0500

    Updates routes and names

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Budgets::Application.routes.draw do
   root :to => 'welcome#index'
   get "/monthly-statistics" => "monthly_statistics#index", as: 'monthly_statistics' 
-  get "monthly-statistics/:year(/:month)" => "monthly_statistics#show", as: 'monthly_statistic'
+  get "/monthly-statistics/:year(/:month)" => "monthly_statistics#show", as: 'monthly_statistic'
 
   resources :budgets
   resources :budget_categories
@@ -13,15 +13,15 @@ Budgets::Application.routes.draw do
   get '/edit-my-account' => 'users#edit'
   get '/admin' => 'users#index', as: 'admin'
   match 'sign-up' => 'users#new', as: 'sign_up', via: [:get, :post]
-  match '/my-budgets/:year/:month' => 'budget_categories#index', as: 'my_budgets', via: [:get, :post]  
+  match '/cash-flow-plans/:year/:month' => 'budget_categories#index', as: 'my_budgets', via: [:get, :post]  
   
   get '/contact' => 'welcome#contact', as: "contact"
   get "/tos" => 'welcome#tos', as: "tos"
   get "/about" => 'welcome#about', as: "about"
 
-  get 'sign-out' => 'sessions#destroy', :as => 'logout'
-  get 'sign-in' => 'sessions#create', :as => 'login'
-  get 'my-account' => 'users#my_account', :as => 'my_account'
+  get 'sign-out' => 'sessions#destroy', as: 'logout'
+  get 'sign-in' => 'sessions#create', as: 'login'
+  get 'my-account' => 'users#my_account', as: 'my_account'
 
   get '/sign-in-up' => 'welcome#sign_in', as: 'sign_in_modal'  
   resources :password_resets, path: 'password-resets'
