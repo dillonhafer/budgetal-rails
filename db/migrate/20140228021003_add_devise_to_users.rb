a335c05ff5dc65fd6efe7638a674d248d54ece54
Dillon Hafer <dh@dillonhafer.com>
2014-02-27 23:37:40 -0500

    Fixes index

--- db/migrate/20140228021003_add_devise_to_users.rb
+++ db/migrate/20140228021003_add_devise_to_users.rb
@@ -38,7 +38,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
       # t.timestamps
     end
 
-    add_index :users, :email,                :unique => true
+    #add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
