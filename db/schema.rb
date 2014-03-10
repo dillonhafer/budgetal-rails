5a8f5fd595ca3092776c91966c6b6a291b52030f
Dillon Hafer <dh@dillonhafer.com>
2014-03-10 13:06:27 -0400

    Updates db schema

--- db/schema.rb
+++ db/schema.rb
@@ -69,28 +69,27 @@ ActiveRecord::Schema.define(version: 20140306011622) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.boolean  "admin"
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",                 default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "password_salt",          limit: nil
-    t.string   "password_hash",          limit: nil
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
