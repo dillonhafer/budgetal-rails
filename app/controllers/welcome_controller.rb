4e61d3ac3f88e33808125ed8aae505e21d1d3870
Dillon Hafer <dh@dillonhafer.com>
2014-09-09 12:28:31 -0400

    Adds privacy page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,6 @@
 class WelcomeController < ApplicationController
   before_filter :new_user
-
-  def index
-  end
-
-  def tos
-  end
-
-  def about
-  end
-
-  def contact
-  end
-
+    
   def new_user
     @user = User.new
   end
