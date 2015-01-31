d2dc0e1080ce5997ec11c4b586124429edfef8b5
Dillon Hafer <dh@dillonhafer.com>
2015-01-31 12:12:41 -0500

    Fixes cache issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,24 @@ class ApplicationController < ActionController::Base
   end  
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :first_name, :last_name) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name, :current_password) }
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(
+        :email,
+        :password,
+        
+        :first_name,
+        :last_name
+      )
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(
+        :email,
+        :password,
+        :password_confirmation,
+        :first_name,
+        :last_name,
+        :current_password
+      )
+    end
   end  
 end
