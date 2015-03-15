a7d6b0effef53c8d61b9bf7a0b422c9374ed15f5
Dillon Hafer <dh@dillonhafer.com>
2015-03-15 13:34:09 -0400

    Adds images precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(vendor/assets/javascript/*.js vendor/assets/stylesheets/*.css vendor/assets/stylesheets/images/*.png)
+Rails.application.config.assets.precompile += %w(vendor/assets/javascript/*.js vendor/assets/stylesheets/*.css vendor/assets/images/*.png)
