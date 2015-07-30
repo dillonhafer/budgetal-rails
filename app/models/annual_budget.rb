5e4ab3a97433d0e90577fe1e78e2b85c6e2aee79
Dillon Hafer <dh@dillonhafer.com>
2015-07-29 19:00:43 -0500

    Remove nested attributes

--- app/models/annual_budget.rb
+++ app/models/annual_budget.rb
@@ -1,9 +1,6 @@
 class AnnualBudget < ActiveRecord::Base
   belongs_to :user
   has_many :annual_budget_items
-
-  accepts_nested_attributes_for :annual_budget_items, allow_destroy: true
-
   validates_presence_of :user_id, :year
   validates_uniqueness_of :year, scope: :user_id
 end
