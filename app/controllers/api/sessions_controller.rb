b4ac5fdf738e88ca0841607470c953d7a703fa2a
Dillon Hafer <dh@dillonhafer.com>
2015-08-01 13:52:01 -0500

    Add login cookies

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Api::SessionsController < Devise::RegistrationsController
   prepend_before_filter :require_no_authentication, only: [:create, :new]
   after_filter :set_csrf_header, only: [:create, :new]
+  after_action :remove_sign_in_cookie, only: [:destroy]
   respond_to :json
 
   def new
@@ -16,6 +17,7 @@ class Api::SessionsController < Devise::RegistrationsController
 
     if resource.valid_password?(params[:user][:password])
       sign_in("user", resource)
+      set_signed_in
       render json: {
         success: true,
         email: resource.email
@@ -27,6 +29,14 @@ class Api::SessionsController < Devise::RegistrationsController
 
   protected
 
+  def remove_sign_in_cookie
+    cookies.delete :signed_in
+  end
+
+  def set_signed_in
+    cookies[:signed_in] = true
+  end
+
   def set_csrf_header
     response.headers['X-CSRF-Token'] = form_authenticity_token
   end
@@ -35,7 +45,7 @@ class Api::SessionsController < Devise::RegistrationsController
     warden.custom_failure!
     render json: {
       success: false,
-      message: "Inncorrect email or password"
+      message: "Incorrect email or password"
     }, status: 401
   end
 end
