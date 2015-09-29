76495c8566830f3347369bd8495fbca7f0866249
Dillon Hafer <dh@dillonhafer.com>
2015-09-29 18:02:52 -0500

    Remove old logic in annual budget controller

--- app/controllers/annual_budgets_controller.rb
+++ app/controllers/annual_budgets_controller.rb
@@ -1,45 +1,26 @@
 class AnnualBudgetsController < AuthenticatedController
   def index
-    @annual_budget = current_user.annual_budgets
-                                 .includes(:annual_budget_items)
-                                 .find_or_create_by(year: year_param)
     respond_to do |f|
       f.html
-      f.json {respond_with @annual_budget, include: :annual_budget_items}
+      f.json { respond_with annual_budget, include: :annual_budget_items }
     end
-  end
-
-  def update
-    @annual_budget = current_user.annual_budgets.includes(:annual_budget_items).find(params[:id])
-    flash[:notice] = if @annual_budget.update_attributes(annual_budget_params)
-                       "Saved budget items"
-                     else
-                       "Something went wrong"
-                     end
   end
 
   private
 
-  def annual_budget_params
-    params.require(:annual_budget).permit(
-      annual_budget_items_attributes: [
-        :id,
-        :name,
-        :amount,
-        :due_date,
-        :paid,
-        :_destroy
-      ])
+  def annual_budget
+    current_user.annual_budgets.includes(:annual_budget_items).find_or_create_by(year: year)
   end
 
-  def year_param
-    year       = params[:year].to_i
-    year_limit = Date.today.advance(years: 2).year
+  def year
+    allowed_year? ? params[:year] : Date.today.year
+  end
 
-    if (2015..year_limit).include?(year)
-      year
-    else
-      Date.today.year
-    end
+  def allowed_year?
+    allowed_years.include?(params[:year].to_i)
+  end
+
+  def allowed_years
+    (2015..Date.today.year+2)
   end
 end
