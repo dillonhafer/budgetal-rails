f51fda3a8ada1363f6e744ef35d0f3bc73a600c2
Dillon Hafer <dh@dillonhafer.com>
2015-10-01 08:37:27 -0500

    Add helper funcitons to js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,6 +38,24 @@ function numberToCurrency(number, dollarSign) {
   return dollarSign + parseFloat(number).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
 }
 
+function monthName(number) {
+  var months = {
+    '1': 'Janurary',
+    '2': 'Feburary',
+    '3': 'March',
+    '4': 'April',
+    '5': 'May',
+    '6': 'June',
+    '7': 'July',
+    '8': 'August',
+    '9': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
+  }
+  return months[number]
+}
+
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
 }
@@ -74,6 +92,13 @@ var AnnualBudgetItemController = {
               dataType: 'json',
               method: 'DELETE'
             })
+  }
+}
+
+var BudgetCategoryController = {
+  find: function(data) {
+    var url = '/budget-category/'+data.year+'/'+data.month+'/'+data.id;
+    return $.ajax({url: url, dataType: 'json'})
   }
 }
 
