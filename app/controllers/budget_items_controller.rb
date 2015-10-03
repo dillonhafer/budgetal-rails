47fae664aa8864935dafc5f8d17fbac9114e7024
Dillon Hafer <dh@dillonhafer.com>
2015-10-03 18:09:29 -0400

    Add json to budget item controller

--- app/controllers/budget_items_controller.rb
+++ app/controllers/budget_items_controller.rb
@@ -1,41 +1,49 @@
 class BudgetItemsController < AuthenticatedController
   respond_to :html, :js, :json
+  helper_method :budget_item
 
   def create
-    category = current_user.budget_categories.find(params[:budget_category_id])
-    item = category.budget_items.new(budget_item_params)
-
-    if item.save
-      render json: item
+    if budget_item.save
+      render :budget_item
     else
-      render json: { errors: item.errors }, status: 422
+      render json: { errors: budget_item.errors }, status: 422
     end
   end
 
   def update
-    item = current_user.budget_items.find(params[:id])
-    if item.update_attributes(budget_item_params)
-      render json: item
+    if budget_item.update_attributes(budget_item_params)
+      render :budget_item
     else
-      render json: { errors: item.errors }, status: 422
+      render json: { errors: budget_item.errors }, status: 422
     end
   end
 
   def destroy
-    @item = current_user.budget_items.find(params[:id])
-    flash[:notice] = "Deleted #{@item.name}"
-    @item.destroy
+    budget_item.destroy
+    respond_with budget_item
   end
 
   def move_item
-    category   = current_user.budget_categories.find(params[:move_category_id])
-    @item      = current_user.budget_items.find(params[:move_item_id])
+    category = current_user.budget_categories.find(params[:move_category_id])
+    @item    = current_user.budget_items.find(params[:move_item_id])
     @original_category = @item.budget_category
     @item.update_attributes(budget_category_id: category.id)
     flash[:notice] = "Moved #{@item.name} to #{category.name}"
   end
 
   private
+
+  def budget_item
+    @budget_item ||= if params[:id]
+      current_user.budget_items.find(params[:id])
+    else
+      category.budget_items.new(budget_item_params)
+    end
+  end
+
+  def category
+    @category ||= current_user.budget_categories.find(params[:budget_category_id])
+  end
 
   def budget_item_params
     params.require(:budget_item).permit(
