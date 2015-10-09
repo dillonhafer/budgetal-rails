0c517acdcec6b35ca04c0d385224fe92a1a8dd6c
Dillon Hafer <dh@dillonhafer.com>
2015-10-09 18:21:26 -0500

    Remove caching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,6 @@ module ApplicationHelper
     Date.new(2012, n.to_i).strftime("%B")
   end
 
-  def assets_cache_key
-    asset_dir = File.join(Rails.root, 'public', 'assets')
-    sha_sum   = %x(find #{asset_dir} -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
-    sha_sum.remove('-').strip
-  end
-
   def devise_reveal_modals
     reveal_modal('signInUp') +
     reveal_modal('forgotPassword')
@@ -25,6 +19,4 @@ module ApplicationHelper
   def link_to_i(icon, text, path, options={})
     link_to("<i class='fi-icon fi-#{icon}'></i> #{text}".html_safe, path, options)
   end
-
-  private
 end
