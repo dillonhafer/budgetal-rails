98f8b6830011672b40566d0845287de6055bd867
Dillon Hafer <dh@dillonhafer.com>
2015-12-21 02:23:20 -0600

    Remove obsolete action from budgets controller

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -6,21 +6,6 @@ class BudgetsController < ApplicationController
     respond_with budget
   end
 
-  def new
-    next_month = Date.today.advance(months: 1)
-
-    if params[:month]
-      next_month = Date.new params[:year], params[:month], 1
-    end
-
-    if current_user.budgets.where(month: next_month.month.to_s, year: next_month.year.to_s).any?
-      redirect_to my_budgets_path(month: next_month.month.to_s, year: next_month.year.to_s)
-    else
-      Budget.create_template(next_month.month, next_month.year, current_user.id)
-      redirect_to my_budgets_path(month: next_month.month.to_s, year: next_month.year.to_s)
-    end
-  end
-
   def update
     if updated_budget.update_attributes(budget_params)
       flash[:notice] = "Updated budget"
