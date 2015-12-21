c0bb33c9ed20d6823a0d637700de52e205a17b6c
Dillon Hafer <dh@dillonhafer.com>
2015-12-21 01:50:46 -0600

    Refactor budget category controller to be more DRY

--- app/controllers/budget_categories_controller.rb
+++ app/controllers/budget_categories_controller.rb
@@ -22,7 +22,7 @@ class BudgetCategoriesController < AuthenticatedController
   def budget
     year  = params[:year]
     month = params[:month]
-    @budget ||= current_user.budgets.includes(:budget_categories).find_by(month: month.to_s, year: year.to_s) || Budget.create_template(month, year, current_user.id)
+    @budget ||= current_user.budgets.includes(:budget_categories).find_by(month: month, year: year) || Budget.create_template(month, year, current_user.id)
   end
 
   def budget_category
@@ -35,16 +35,27 @@ class BudgetCategoriesController < AuthenticatedController
 
   def check_date
     year  = params[:year].to_i
-    month = params[:month].to_i
-    if year > Time.now.advance(years: 1).year
-      flash[:error] = "You can't go that far into the future. Who's to say we'd still be around?"
-      redirect_to root_path
-    elsif year < 2013
-      flash[:error] = "We didn't exist back then, I don't think you'll find a budget there."
-      redirect_to root_path
-    elsif !(1..12).include?(month)
-      flash[:error] = "You do know there are only 12 months in a year right?"
-      redirect_to root_path
-    end
+    error = if year_too_great?(params[:year])
+              "You can't go that far into the future. Who's to say we'd still be around?"
+            elsif year_too_small?(params[:year])
+              "We didn't exist back then, I don't think you'll find a budget there."
+            elsif invalid_month?(params[:month])
+              "You do know there are only 12 months in a year right?"
+            end
+
+    flash[:error] = error
+    redirect_to root_path unless flash[:error].blank?
+  end
+
+  def year_too_great?(year)
+    year.to_i > 1.year.from_now.year
+  end
+
+  def year_too_small?(year)
+    year.to_i < 2013
+  end
+
+  def invalid_month?(month)
+    (1..12).exclude?(month.to_i)
   end
 end
