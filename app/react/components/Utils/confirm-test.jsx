f94915435fa6fe779c6ad5390e5e296c372bb5b9
Dillon Hafer <dh@dillonhafer.com>
2015-12-23 00:49:16 -0600

    Fix case in component

--- app/react/components/Utils/confirm-test.jsx
+++ app/react/components/Utils/confirm-test.jsx
@@ -1,7 +1,7 @@
 import React from 'react/addons';
 const TestUtils = React.addons.TestUtils;
 import expect from 'expect';
-import Confirm from './Confirm';
+import Confirm from './confirm';
 
 describe('Confirm', () => {
   const component = TestUtils.renderIntoDocument(<Confirm name='Test Name' hidden={false} delete={function() {}} cancel={function() {}}/>);
