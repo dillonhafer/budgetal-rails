6f5dfc968573bfa504b1250b27ded2c22d4e98bc
Dillon Hafer <dh@dillonhafer.com>
2015-12-23 01:21:42 -0600

    Add test for selectedValue

--- app/react/components/Utils/helpers-test.jsx
+++ app/react/components/Utils/helpers-test.jsx
@@ -23,18 +23,37 @@ describe('Helpers', () => {
   describe('numberToCurrency', () => {
     it('returns a buck without any arguments', () => {
       expect(numberToCurrency('24.99')).toBe('$24.99');
-    })
+    });
 
     it('returns a custom symbol', () => {
       expect(numberToCurrency('24.99', '&')).toBe('&24.99');
-    })
+    });
 
     it('adds decimal points to integers', () => {
       expect(numberToCurrency(24)).toBe('$24.00');
-    })
+    });
 
     it('returns 0.00 if a number cannot be found', () => {
       expect(numberToCurrency('abc')).toBe('$0.00');
-    })
-  })
+    });
+  });
+
+  describe('selectedValue', () => {
+    it('returns the value of a select tag by CSS selector', () => {
+      var select = document.createElement('select');
+      select.className = 'month-dropdown';
+      ['Janurary', 'July', 'March'].forEach(function(month) {
+        var option = document.createElement('option');
+        option.value = month;
+        option.innerHTML = month;
+        if (month == 'March') {
+          option.setAttribute('selected', 'selected');
+        }
+        select.appendChild(option);
+      })
+      document.body.appendChild(select)
+
+      expect(selectedValue('.month-dropdown')).toBe('March');
+    });
+  });
 });
