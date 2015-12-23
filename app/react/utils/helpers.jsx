af88f127adf38e4ec820f85a045273d4eabd2174
Dillon Hafer <dh@dillonhafer.com>
2015-12-23 17:53:32 -0600

    Use natural index for month name lookups

--- app/react/utils/helpers.jsx
+++ app/react/utils/helpers.jsx
@@ -1,19 +1,19 @@
 export function monthName(number) {
-  var months = {
-    '1': 'Janurary',
-    '2': 'Feburary',
-    '3': 'March',
-    '4': 'April',
-    '5': 'May',
-    '6': 'June',
-    '7': 'July',
-    '8': 'August',
-    '9': 'September',
-    '10': 'October',
-    '11': 'November',
-    '12': 'December'
-  }
-  return months[number]
+  var months = [
+    'Janurary',
+    'Feburary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+  return months[parseInt(number)-1]
 }
 
 export function selectedValue(selector) {
