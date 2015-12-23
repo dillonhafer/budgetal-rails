442a707ddbc462f5d50a65a1e634c78ff2b75224
Dillon Hafer <dh@dillonhafer.com>
2015-12-23 01:11:49 -0600

    Remove old helper

--- app/react/components/Utils/numbers.js
+++
@@ -1,9 +0,0 @@
-function numberToCurrency(number, dollarSign) {
-  if (dollarSign === undefined) {
-    dollarSign = '$'
-  }
-  if (isNaN(parseFloat(number))) {
-    number = 0
-  }
-  return dollarSign + parseFloat(number).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-}
