f485b77b7810453d19cc1745121f8526dedb6ff7
Dillon Hafer <dh@dillonhafer.com>
2015-12-24 16:12:40 -0500

    Refactor to remove old function and tidy up

--- app/react/components/annualBudgetItems/item_form.js.jsx
+++ app/react/components/annualBudgetItems/item_form.js.jsx
@@ -8,52 +8,36 @@ export default class AnnualBudgetItemForm extends React.Component {
     super(props);
   }
 
-  message = () => {
-    let message = `Are you sure you want to delete ${this.props.budgetItem.name}?
-          This cannot be undone.`
-    return message
+  static propTypes = {
+    budgetItem: React.PropTypes.object.isRequired,
+    updateForm: React.PropTypes.func.isRequired,
+    saveForm: React.PropTypes.func.isRequired,
+    delete: React.PropTypes.func.isRequired
   }
 
   updateForm = (e) => {
-    var newValue = (e.target.name === 'paid') ? e.target.checked : e.target.value
-    this.props.budgetItem[e.target.name] = newValue
-    this.props.updateForm(this.props.index, this.props.budgetItem)
+    var newValue = (e.target.name === 'paid') ? e.target.checked : e.target.value;
+    this.props.budgetItem[e.target.name] = newValue;
+    this.props.updateForm(this.props.index, this.props.budgetItem);
   }
 
-  deleteItem = (e) => {
-    e.preventDefault()
-    if (!this.props.budgetItem.id || confirm(this.message())) {
-      this.props.deleteForm({
-        annual_budget_item: this.props.budgetItem,
-        index: this.props.index
-      })
-    }
-  }
-
-  saveItem = (e) => {
-    e.preventDefault()
+  saveForm = (e) => {
+    e.preventDefault();
     this.props.saveForm({
       annual_budget_item: this.props.budgetItem,
       index: this.props.index
-    })
+    });
   }
 
-  paid = () => {
-    var text = this.props.budgetItem.paid ? 'Paid' : 'Not Paid'
-    var css  = 'label radius '
-    css += this.props.budgetItem.paid ? 'success' : 'alert'
-    return (<span className={css}>{text}</span>)
-  }
-
-  _delete = (e) => {
-    e.preventDefault()
-    this.props.delete(this.props.budgetItem, this.props.index)
+  deleteForm = (e) => {
+    e.preventDefault();
+    this.props.delete(this.props.budgetItem, this.props.index);
   }
 
   render() {
-    let item = this.props.budgetItem
+    let item = this.props.budgetItem;
     return (
-      <form onSubmit={this.saveItem} data-abide>
+      <form onSubmit={this.saveForm} data-abide>
         <div className='row'>
           <div className='large-4 columns'>
             <InputField type='text' name='name' onChange={this.updateForm} value={item.name} placeholder='Name' errors={item.errors} />
@@ -70,7 +54,7 @@ export default class AnnualBudgetItemForm extends React.Component {
           <div className='large-3 columns'>
             <button type='submit' title='Save Item' className='tiny success radius button'><i className='fi-icon fi-check'></i> Save</button>
             &nbsp;
-            <a href='#' onClick={this._delete} title='Delete Item' className='tiny alert radius button'><i className='fi-icon fi-trash'></i> Delete</a>
+            <a href='#' onClick={this.deleteForm} title='Delete Item' className='tiny alert radius button'><i className='fi-icon fi-trash'></i> Delete</a>
           </div>
         </div>
       </form>
