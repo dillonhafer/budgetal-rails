2188cbdeccac1ff0e43392c47119b9382cab81b1
Dillon Hafer <dh@dillonhafer.com>
2015-12-24 13:10:33 -0500

    Add test for annual budget item

---
+++ app/react/components/annualBudgetItems/item-test.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+import AnnualBudgetItem from './item';
+
+describe('AnnualBudgetItem', () => {
+  let budgetItem  = {name: 'Amazon', amount: 12.12, due_date: '2015-3-12', paid: true}
+  var markup      = React.renderToStaticMarkup(<AnnualBudgetItem budgetItem={budgetItem} />);
+  const component = TestUtils.renderIntoDocument(<AnnualBudgetItem budgetItem={budgetItem} />);
+
+  it('renders', () => {
+    var item = TestUtils.findRenderedComponentWithType(component, AnnualBudgetItem);
+    expect(item).toExist();
+  });
+
+  it('has a pretty title', () => {
+    expect(markup).toContain('<li class="title">Amazon</li>');
+  });
+
+  it('has a formatted amount', () => {
+    expect(markup).toContain('<li class="price">$12.12</li>');
+  });
+
+  it('has a formatted due date', () => {
+    expect(markup).toContain('<li class="description"><b>Due:</b> March 12, 2015</li>');
+  });
+
+  it('has an amount per month', () => {
+    expect(markup).toContain('<li class="bullet-item">$1.01 / month</li>');
+  });
+
+  it('has the correct paid label', () => {
+    expect(markup).toContain('<li class="bullet-item"><span class="label radius success">Paid</span></li>');
+  });
+});
