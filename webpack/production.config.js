f06d8a607fa57ae94baa99fa5eea4bf67c5ac08b
Dillon Hafer <dh@dillonhafer.com>
2015-12-23 23:04:20 -0500

    Add production webpack options

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,3 +1,28 @@
-const config = require('./../webpack.config');
+'use strict';
+const baseConfig = require('./../webpack.config');
+var _ = require('lodash');
+var webpack = require('webpack');
+var CleanPlugin = require('clean-webpack-plugin');
+
+var config = _.merge({}, baseConfig, {
+  cache: false,
+  plugins: [
+    {
+      new webpack.NoErrorsPlugin(),
+      new CleanPlugin(['build']),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        output: {
+          comments: false
+        },
+        compress: {
+          warnings: false
+        }
+      }),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.AggressiveMergingPlugin(),
+    }
+  ]
+}
 
 module.exports = config;
