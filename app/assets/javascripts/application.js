499db3e038890f6640a895e8c5fb3620a6bf6504
Dillon Hafer <dh@dillonhafer.com>
2016-01-01 17:31:51 -0600

    Replace jQuery animation with css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,16 +4,6 @@
 //= require react_integration
 //= require react_bundle
 
-function showMessage(message) {
-  var holder = document.querySelector('.flash-holder');
-  var box = document.createElement('div');
-  box.setAttribute('class', 'flash-box');
-  box.innerHTML = message;
-  holder.appendChild(box);
-
-  $(".flash-box").fadeIn(400).delay(1700).fadeOut(250, function() { $(this).remove(); });
-}
-
 $(document).on('focus', '.get-date', function(e) {
   $(this).minical({
     date_changed: function() {
--- app/frontend/index.js
+++ app/frontend/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 window.React = React;
 window.ReactDOM = ReactDOM;
 
+import {showMessage} from './utils/flash-box';
+window.showMessage = showMessage;
+
 import './assets/stylesheets/app';
 
 import CashFlowPlans from './components/cashFlowPlans/cash_flow_plans';
@@ -20,14 +23,8 @@ registerComponent('Sessions', Sessions);
 document.addEventListener('DOMContentLoaded', function() {
   var box = document.querySelector('.flash-box');
   if (box) {
-    var message = box.innerHTML;
-    box.parentNode.removeChild(box);
-    showMessage(message);
-  }
-});
-
-document.addEventListener('click', function(e) {
-  if (e.target.classList.contains('flash-box')) {
-    e.target.parentNode.removeChild(e.target);
+   var message = box.innerHTML;
+   box.parentNode.removeChild(box);
+   showMessage(message);
   }
 });
