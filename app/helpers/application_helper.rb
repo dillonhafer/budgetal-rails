3c1d67b5649502d9b6607ccf604115da11817a61
Dillon Hafer <dh@dillonhafer.com>
2016-01-01 16:13:29 -0600

    Remove old helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,14 +10,6 @@ module ApplicationHelper
     content_for(:title, word)
   end
 
-  def month_name(n)
-    Date.new(2012, n.to_i).strftime("%B")
-  end
-
-  def reveal_modal(id,size='small')
-    content_tag(:div, '', class: "reveal-modal #{size}", id: id.to_s, data: {reveal:''})
-  end
-
   def link_to_i(icon, text, path, options={})
     link_to("<i class='fi-icon fi-#{icon}'></i> #{text}".html_safe, path, options)
   end
