98e2f9757dacc73766a32a2f0d53eea59b19ba66
Dillon Hafer <dh@dillonhafer.com>
2016-01-02 14:27:05 -0600

    Don't export nonGet method

--- app/frontend/data/api.jsx
+++ app/frontend/data/api.jsx
@@ -1,18 +1,4 @@
 export default {
-  nonGetRequest(method, path, body) {
-    let meta = document.querySelector('meta[name="csrf-token"]')
-    let csrfToken = meta ? meta.content : '';
-    return fetch(path, {
-      method: method,
-      credentials: 'include',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': csrfToken
-      },
-      body: JSON.stringify(body)
-    }).then((r) => r.json());
-  },
   getRequest(path) {
     return fetch(path, {
       method: 'GET',
@@ -23,15 +9,30 @@ export default {
       }}).then((r) => r.json());
   },
   postRequest(path, body={}) {
-    return module.exports.nonGetRequest('POST', path, body);
+    return nonGetRequest('POST', path, body);
   },
   putRequest(path, body={}) {
-    return module.exports.nonGetRequest('PUT', path, body);
+    return nonGetRequest('PUT', path, body);
   },
   deleteRequest(path) {
-    return module.exports.nonGetRequest('DELETE', path);
+    return nonGetRequest('DELETE', path);
   },
   patchRequest(path) {
-    return module.exports.nonGetRequest('PATCH', path, body);
+    return nonGetRequest('PATCH', path, body);
   }
+}
+
+function nonGetRequest(method, path, body) {
+  let meta = document.querySelector('meta[name="csrf-token"]')
+  let csrfToken = meta ? meta.content : '';
+  return fetch(path, {
+    method: method,
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': csrfToken
+    },
+    body: JSON.stringify(body)
+  }).then((r) => r.json());
 }
