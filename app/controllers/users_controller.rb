a38f88c33c3d7522b8994ecb91175b69d5474afe
Dillon Hafer <dh@dillonhafer.com>
2016-01-07 21:25:12 -0600

    Users controller inherits from authenticated controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersController < AuthenticatedController
   def past_expenses
     render json: current_user.past_expenses(params[:name])
   end
