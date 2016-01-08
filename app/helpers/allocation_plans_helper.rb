00fa338e33277376867f092caa186e196c3b1762
Dillon Hafer <dh@dillonhafer.com>
2016-01-07 21:22:07 -0600

    Remove old helper

--- app/helpers/allocation_plans_helper.rb
+++ app/helpers/allocation_plans_helper.rb
@@ -1,14 +1,4 @@
 module AllocationPlansHelper
-  def color_for_number(number)
-    if number.to_f > 0.00
-      'green'
-    elsif number.to_f == 0.00
-      'blue'
-    else
-      'red'
-    end
-  end
-
   def allocation_item(items, id)
     items.detect {|item| item.budget_item_id == id} || OpenStruct.new(id: '', amount_budgeted: 0.00)
   end
