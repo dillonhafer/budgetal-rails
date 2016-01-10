4b0075dffcc9d38f8972a01a6834a488c898b163
Dillon Hafer <dh@dillonhafer.com>
2016-01-09 20:49:19 -0600

    Add helper to find pre-built asset hash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,12 @@
 module ApplicationHelper
-  def webpack_hash(ext)
-    build_path = File.join(Rails.root, 'public', 'assets', "*.#{ext}")
-    files      = Dir[build_path].sort_by(&File.method(:mtime))
-    asset      = Pathname.new(files.last).basename
-    "/assets/#{asset}"
-  end
-
-  def webpack_stylesheet_path
-    webpack_hash('css')
-  end
-
-  def webpack_javascript_path
-    webpack_hash('js')
+  def webpack_filename
+    manifest_path = File.join(Rails.root, 'public', 'assets', 'manifest')
+    if File.exists?(manifest_path)
+      sha = IO.binread(manifest_path)
+      "/assets/main-#{sha}"
+    else
+      '/assets/main'
+    end
   end
 
   def link_to_i(icon, text, path, options={})
