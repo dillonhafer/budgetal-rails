e6642a87dacf198194ef1a9af00af30459416a2a
Dillon Hafer <dh@dillonhafer.com>
2016-01-10 00:30:21 -0600

    Chomp the newlines!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def webpack_filename
     manifest_path = File.join(Rails.root, 'public', 'assets', 'manifest')
     if File.exists?(manifest_path)
-      sha = IO.binread(manifest_path)
+      sha = IO.binread(manifest_path).chomp
       "/assets/main-#{sha}"
     else
       '/assets/main'
