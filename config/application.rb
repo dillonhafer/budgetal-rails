c143264e5ce4ad499d5d4ae73a56a914d0bcd622
Dillon Hafer <dh@dillonhafer.com>
2016-01-15 14:20:36 -0600

    Only set env var if it's missing

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,7 @@ module Budgets
     config.before_configuration do
       env_file = File.join(Rails.root, '.env')
       YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
+        ENV[key.to_s] ||= value
       end if File.exists?(env_file)
     end
   end
