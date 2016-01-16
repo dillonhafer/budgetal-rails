209c3ae0e22b79c9bb478a78ef1840e15f659bc5
Dillon Hafer <dh@dillonhafer.com>
2016-01-16 16:21:04 -0600

    Clean up rails spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
@@ -6,7 +5,6 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'database_cleaner'
 require 'support/hide_seed'
-
 
 Selenium::WebDriver::Chrome::Service.executable_path = ENV.fetch('CHROME_DRIVER', '/usr/local/bin/chromedriver')
 Capybara.register_driver :chrome do |app|
@@ -17,18 +15,15 @@ Capybara.javascript_driver = :chrome
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.success_color = :blue
+  config.order         = :random
   config.detail_color  = :blue
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.success_color = :blue
 
   config.include FactoryGirl::Syntax::Methods
+  config.infer_spec_type_from_file_location!
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
+  # Database Cleaner
   config.use_transactional_fixtures = false
-
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
@@ -41,19 +36,4 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
-  config.order = :random
 end
