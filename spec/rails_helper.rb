fd9c99870862e79c02c2c217743d6385e24eacde
Dillon Hafer <dh@dillonhafer.com>
2016-01-17 15:07:33 -0600

    Require capybara/rspec per documentation
      https://github.com/jnicklas/capybara#using-capybara-with-rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
+require 'capybara/rspec'
 require 'database_cleaner'
 require 'support/hide_seed'
 
