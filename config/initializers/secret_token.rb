14efe1334285e0604efd5124401533465f7b46df
Dillon Hafer <dh@dillonhafer.com>
2016-01-18 02:01:35 -0600

    No longer need secret base as cookies are not and should not be used

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Budgets::Application.config.secret_key_base = ENV.fetch('SECRET_KEY_BASE')
+Budgets::Application.config.secret_key_base = ENV.fetch('dont', 'use cookies')
