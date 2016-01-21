a8e8aeed044ddb66b07148f889b42853662dd18d
Dillon Hafer <dh@dillonhafer.com>
2016-01-21 16:40:52 -0600

    Remove named routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,25 +30,25 @@ Budgets::Application.routes.draw do
     get '/budget-categories/:id/import' => 'budget_categories#import'
     get '/budget-categories/:year/:month/:id' => 'budget_categories#show'
     get '/budget-categories/:id' => 'budget_categories#show'
-    match '/cash-flow-plans/:year/:month' => 'budget_categories#index', as: 'my_budgets', via: [:get, :post]
+    match '/cash-flow-plans/:year/:month' => 'budget_categories#index', via: [:get, :post]
 
     resources :budget_items, path: 'budget-items', only: [:create, :update, :destroy]
-    patch '/move-budget-item' => 'budget_items#move_item', as: 'move_item'
+    patch '/move-budget-item' => 'budget_items#move_item'
 
     resources :budget_item_expenses, path: 'budget-item-expenses', only: [:create, :update, :destroy]
 
-    get '/annual-budgets/:year' => 'annual_budgets#show', as: 'annual_budgets'
+    get '/annual-budgets/:year' => 'annual_budgets#show'
     resources :annual_budget_items, path: '/annual-budget-items', only: [:create, :update, :destroy]
 
     get '/allocation-plans/:id' => 'allocation_plans#show'
     resources :allocation_plans, path: '/allocation-plans/:year/:month', only: [:index, :create, :update]
     resources :allocation_plan_budget_items, path: '/allocation-plan-budget-items', only: [:create, :update]
 
-    get "/monthly-statistics/:year/:month" => "monthly_statistics#show", as: 'monthly_statistic'
+    get "/monthly-statistics/:year/:month" => "monthly_statistics#show"
     get "/monthly-statistics-budget/:year/:month" => "monthly_statistics#budget"
 
-    get '/my-account' => 'users#my_account', as: 'my_account'
-    get '/past-expenses/:name' => 'users#past_expenses', as: 'past_expenses'
+    get '/my-account' => 'users#my_account'
+    get '/past-expenses/:name' => 'users#past_expenses'
 
     get '/admin/users' => 'admin#users'
   end
