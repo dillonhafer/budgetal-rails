241d9ea1a524963bca17d10bc05af6d2f0408062
Dillon Hafer <dh@dillonhafer.com>
2016-01-24 22:59:24 -0600

    Remove old action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,8 +2,4 @@ class Api::UsersController < AuthenticatedController
   def past_expenses
     render json: current_user.past_expenses(params[:name])
   end
-
-  def my_account
-	  render json: current_user
-  end
 end
