71b289ed1f6191fd931bbe6c29ef1f968fbc93a4
Dillon Hafer <dh@dillonhafer.com>
2016-01-29 17:46:10 -0600

    Have webpack build/server index.html from template

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -4,6 +4,7 @@ var _ = require('lodash');
 var webpack = require('webpack');
 var WebpackNotifierPlugin = require('webpack-notifier');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var config = _.merge({}, baseConfig, {
   cache: true,
@@ -16,7 +17,11 @@ var config = _.merge({}, baseConfig, {
     new webpack.ProvidePlugin({
       'Promise': 'exports?global.Promise!es6-promise',
       'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    })
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../public/index.html',
+      template: __dirname + '/templates/dev/index.html'
+    }),
   ]
 })
 module.exports = config;
