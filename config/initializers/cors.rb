183d932cb8ae4c09c52c3c0f91e6448154cc4c57
Dillon Hafer <dh@dillonhafer.com>
2016-01-31 11:57:26 -0600

    Allow multiple domain names in CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
    allow do
-     origins ENV.fetch('CORS', 'localhost:9999')
+     origins ENV.fetch('CORS', 'localhost:9999').split
 
      resource '/api/*',
        headers: :any,
