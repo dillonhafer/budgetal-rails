0db86f7b150dcfe97c1dfd34780334cb65ecabf7
Dillon Hafer <dh@dillonhafer.com>
2016-02-12 08:45:48 -0600

    Clean up item form

--- ios-client/app/Views/budgets/budget-items/form.js
+++ ios-client/app/Views/budgets/budget-items/form.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var React = require('react-native');
-var {
-  AlertIOS,
-  SwitchIOS,
+let React = require('react-native');
+let {
   Text,
   TextInput,
   TouchableHighlight,
@@ -11,10 +9,10 @@ var {
 } = React;
 
 import styles from './styles';
-import {numberToCurrency} from '../../../Utils/ViewHelpers';
+import {numberToCurrency, showErrors} from '../../../Utils/ViewHelpers';
 import {updateItem, createItem} from '../../../Data/budget_item';
 
-var BudgetItemForm = React.createClass({
+let BudgetItemForm = React.createClass({
   getInitialState() {
     return ({
       budgetItem: {name: '', amount_budgeted: ''}
@@ -27,27 +25,24 @@ var BudgetItemForm = React.createClass({
     }
   },
   saveItem: async function() {
+    this.refs.name.blur();
+    this.refs.amount.blur();
     let strategy = (this.state.budgetItem.id === undefined) ? createItem : updateItem;
     let data = {budget_category_id: this.state.budgetItem.category_id, budget_item: this.state.budgetItem};
-    this.blurInputs();
 
     try {
       let resp = await strategy(data);
       if (resp !== null && resp.errors === undefined) {
         this.props.navigator.props.popRoute();
       } else {
-      // handle validation errors
+        showErrors(resp.errors);
       }
     } catch (err) {
       this.props.navigator.props.signOut();
     }
   },
-  blurInputs() {
-    this.refs.name.blur();
-    this.refs.amount.blur();
-  },
   render() {
-    var b = this.state.budgetItem;
+    let b = this.state.budgetItem;
     return (
       <View style={[styles.container, styles.form]}>
         <Text style={styles.label}>Budget Item</Text>
@@ -56,21 +51,21 @@ var BudgetItemForm = React.createClass({
             <Text style={styles.label}>Name</Text>
           </View>
           <View style={styles.right}>
-            <TextInput style={styles.inputs} placeholder='Name (Life Insurrance)'
+            <TextInput style={styles.inputs} placeholder='(Life Insurrance)'
                        autoCapitalize='words'
                        ref='name'
                        onFocus={this.inputFocus}
-                       onChangeText={(name)=> this.setState({budgetItem: Object.assign(b,{name})})}
+                       onChangeText={(name)=> this.setState({budgetItem: Object.assign({}, b, {name})})}
                        defaultValue={b.name} />
           </View>
         </View>
 
         <View style={styles.paidSwitch}>
           <View style={styles.column}>
-            <Text style={styles.label}>Amount</Text>
+            <Text style={styles.label}>Budgeted</Text>
           </View>
           <View style={styles.right}>
-            <TextInput style={styles.inputs} placeholder='Amount ($3.00)'
+            <TextInput style={styles.inputs} placeholder='($42.00)'
                        keyboardType='decimal-pad'
                        ref="amount"
                        onFocus={this.inputFocus}
