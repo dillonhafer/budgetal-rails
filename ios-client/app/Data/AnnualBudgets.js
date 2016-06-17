88bc1b66f546f66cdbc5775e38c2e6945022c458
Dillon Hafer <dh@dillonhafer.com>
2016-06-16 22:18:19 -0500

    Fix API functions for annual budgets

--- ios-client/app/Data/AnnualBudgets.js
+++ ios-client/app/Data/AnnualBudgets.js
@@ -3,13 +3,13 @@ module.exports = {
   all(year) {
     return getRequest(`/annual-budgets/${year}`);
   },
-  save(annual_budget_item) {
-    return putRequest(`/annual-budget-items/${annual_budget_item.id}`, {annual_budget_item});
+  update(params) {
+    return putRequest(`/annual-budget-items/${params.annual_budget_item.id}`, params.annual_budget_item);
   },
-  delete(annual_budget_item) {
-    return deleteRequest(`/annual-budget-items/${annual_budget_item.id}`, {annual_budget_item});
+  deleteItem(id) {
+    return deleteRequest(`/annual-budget-items/${id}`);
   },
   create(annual_budget_item) {
-    return postRequest(`/annual-budget-items/${annual_budget_item.id}`, {annual_budget_item});
+    return postRequest(`/annual-budget-items`, annual_budget_item);
   }
 };
