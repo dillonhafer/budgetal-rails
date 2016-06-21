c54be70b6a7c49d402a1b5e0508c258e31d967bc
Dillon Hafer <dh@dillonhafer.com>
2016-06-21 15:14:56 -0500

    Don't use relatie assets

--- docs/config.rb
+++ docs/config.rb
@@ -34,7 +34,7 @@ configure :build do
   # out the following two lines has been known to help
   activate :minify_css
   activate :minify_javascript
-  activate :relative_assets
+  # activate :relative_assets
   # activate :asset_hash
   # activate :gzip
 end
