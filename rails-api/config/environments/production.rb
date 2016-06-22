63c9a2ac43f766fbfd94c3554a3894f2888b4eae
Dillon Hafer <dh@dillonhafer.com>
2016-06-22 12:29:04 -0500

    Add sendgrid to production

--- rails-api/config/environments/production.rb
+++ rails-api/config/environments/production.rb
@@ -51,8 +51,18 @@ Budgets::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
   config.action_mailer.default_url_options = { host: "www.budgetal.com" }
-  config.action_mailer.deliver_method :sendmail
-  ActionMailer::Base.smtp_settings[:openssl_verify_mode] = 'none'
+  config.action_mailer.deliver_method :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'budgetal.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   # config.i18n.fallbacks = true
