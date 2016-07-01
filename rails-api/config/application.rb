6fc187424f9bddf2f1c7f59879044e0f0c2f9aa0
Dillon Hafer <dh@dillonhafer.com>
2016-07-01 17:32:48 -0500

    Use API controller

--- rails-api/config/application.rb
+++ rails-api/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Budgets
   class Application < Rails::Application
+    config.api_only = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
