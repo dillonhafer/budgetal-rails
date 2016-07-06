97abb3f60901f5637cd0f0496024387cc4e6b833
Dillon Hafer <dh@dillonhafer.com>
2016-07-06 08:27:29 -0500

    Remove unused tag

--- rails-api/spec/features/budget_spec.rb
+++ rails-api/spec/features/budget_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'support/feature_helper'
 
-feature "Budget navigation", :feature, :js do
+feature "Budget navigation", :js do
   scenario "A user can visit a budgets when they done exist" do
     user = login
     visit root_path
