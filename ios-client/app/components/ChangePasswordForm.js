0f19f80e52e832d30517b4b852859cf7044ebcbc
Dillon Hafer <dh@dillonhafer.com>
2016-07-17 15:12:15 -0500

    Update styles for change password form

--- ios-client/app/components/ChangePasswordForm.js
+++ ios-client/app/components/ChangePasswordForm.js
@@ -1,55 +1,17 @@
 import React, {Component} from 'react'
-import {
-  Image,
-  Dimensions,
-  LayoutAnimation,
-  Text,
-  TouchableOpacity,
-  TouchableHighlight,
-  View
-} from 'react-native'
+import {LayoutAnimation,} from 'react-native'
 
 import {showErrors} from '../utils/ViewHelpers';
 import {alert} from '../utils/window';
 import {changePassword} from '../data/Users';
-import FormInput from './FormInput';
-
-import StyleSheet from './StyleSheet'
-const {width,height} = Dimensions.get('window');
-const styles = StyleSheet.create({
-  label: {
-    fontSize: 13,
-    marginLeft: 10,
-    marginBottom: 4,
-    color: '$formLabel'
-  },
-  form: {
-    flex: 1,
-    paddingTop: 20,
-    backgroundColor: '$formBackground'
-  },
-  text: {
-    color: '$blue',
-    textAlign: 'center',
-    marginTop: 4,
-  },
-  saveButton: {
-    marginTop: 20,
-  },
-  saveButtonText: {
-    textAlign: 'center',
-    backgroundColor: '$white',
-    color: '$blue',
-    margin: 0,
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 40,
-    borderColor: '$grayBorder',
-    borderTopWidth: 1,
-    borderBottomWidth: 1,
-  },
-});
+import {
+  FormContainer,
+  FormInput,
+  FormLabel,
+  InputSeparator,
+  InputContainer,
+  InputButton,
+} from './form-components'
 
 class ChangePasswordForm extends Component {
   constructor(props) {
@@ -97,51 +59,50 @@ class ChangePasswordForm extends Component {
   saveButton = () => {
     LayoutAnimation.easeInEaseOut();
     if (this.validForm()) {
-      return (
-        <TouchableHighlight
-          style={styles.saveButton}
-          underlayColor={'#6699ff'}
-          onPress={this.saveUser}>
-          <Text style={styles.saveButtonText}>Change Password</Text>
-        </TouchableHighlight>
-      )
+      return <InputButton onPress={this.saveUser} text='Change Password' />
     }
   }
 
   render() {
     return (
-      <View style={styles.form}>
-        <Text style={styles.label}>ACCOUNT PASSWORD</Text>
-        <FormInput placeholder='New Password'
-                   required={true}
-                   format='password'
-                   inputType='password'
-                   value={this.state.user.password}
-                   onChangeText={(password)=> this.setState({user: Object.assign({}, this.state.user, {password})})}
-                   label='New Password'
-                   returnKeyType='next' />
+      <FormContainer>
+        <FormLabel label='ACCOUNT PASSWORD' />
+        <InputContainer>
+          <FormInput placeholder='New Password'
+                     required={true}
+                     format='password'
+                     inputType='password'
+                     value={this.state.user.password}
+                     onChangeText={(password)=> this.setState({user: Object.assign({}, this.state.user, {password})})}
+                     label='New Password'
+                     returnKeyType='next' />
 
-       <FormInput placeholder='Confirm Password'
-                  required={true}
-                  format='password_confirmation'
-                  inputType='password'
-                  password={this.state.user.password}
-                  value={this.state.user.password_confirmation}
-                  onChangeText={(password_confirmation)=> this.setState({user: Object.assign({}, this.state.user, {password_confirmation})})}
-                  label='Confirm'
-                  returnKeyType='next' />
+         <InputSeparator />
 
-       <FormInput placeholder='Current Password'
-                  required={true}
-                  format='password'
-                  inputType='password'
-                  value={this.state.currentPassword}
-                  onChangeText={(currentPassword)=> this.setState({currentPassword})}
-                  label='Password'
-                  returnKeyType='done' />
+         <FormInput placeholder='Confirm Password'
+                    required={true}
+                    format='password_confirmation'
+                    inputType='password'
+                    password={this.state.user.password}
+                    value={this.state.user.password_confirmation}
+                    onChangeText={(password_confirmation)=> this.setState({user: Object.assign({}, this.state.user, {password_confirmation})})}
+                    label='Confirm'
+                    returnKeyType='next' />
+
+         <InputSeparator />
+
+         <FormInput placeholder='Current Password'
+                    required={true}
+                    format='password'
+                    inputType='password'
+                    value={this.state.currentPassword}
+                    onChangeText={(currentPassword)=> this.setState({currentPassword})}
+                    label='Password'
+                    returnKeyType='done' />
+        </InputContainer>
 
         {this.saveButton()}
-      </View>
+      </FormContainer>
     )
   }
 }
