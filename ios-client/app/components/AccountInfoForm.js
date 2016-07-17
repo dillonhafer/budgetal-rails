daaa60299f7fb939e8d8f7923b61004d6e127480
Dillon Hafer <dh@dillonhafer.com>
2016-07-17 15:16:07 -0500

    Clean up account info form

--- ios-client/app/components/AccountInfoForm.js
+++ ios-client/app/components/AccountInfoForm.js
@@ -1,54 +1,16 @@
 import React, {Component} from 'react'
-import {
-  Image,
-  Dimensions,
-  LayoutAnimation,
-  Text,
-  TouchableOpacity,
-  TouchableHighlight,
-  View
-} from 'react-native'
+import {LayoutAnimation,} from 'react-native'
 
 import {showErrors} from '../utils/ViewHelpers';
 import {saveUser} from '../data/Users';
-import FormInput from './FormInput';
-
-import StyleSheet from './StyleSheet'
-const {width,height} = Dimensions.get('window');
-const styles = StyleSheet.create({
-  label: {
-    fontSize: 13,
-    marginLeft: 10,
-    marginBottom: 4,
-    color: '$formLabel'
-  },
-  form: {
-    flex: 1,
-    paddingTop: 20,
-    backgroundColor: '$formBackground'
-  },
-  text: {
-    color: '$blue',
-    textAlign: 'center',
-    marginTop: 4,
-  },
-  saveButton: {
-    marginTop: 20,
-  },
-  saveButtonText: {
-    textAlign: 'center',
-    backgroundColor: '$white',
-    color: '$blue',
-    margin: 0,
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 40,
-    borderColor: '$grayBorder',
-    borderTopWidth: 1,
-    borderBottomWidth: 1,
-  },
-});
+import {
+  FormContainer,
+  FormInput,
+  FormLabel,
+  InputSeparator,
+  InputContainer,
+  InputButton,
+} from './form-components'
 
 class AccountInfoForm extends Component {
   constructor(props) {
@@ -95,62 +57,63 @@ class AccountInfoForm extends Component {
   saveButton = () => {
     LayoutAnimation.easeInEaseOut();
     if (this.validForm()) {
-      return (
-        <TouchableHighlight
-          style={styles.saveButton}
-          underlayColor={'#6699ff'}
-          onPress={this.saveUser}>
-          <Text style={styles.saveButtonText}>Save</Text>
-        </TouchableHighlight>
-      )
+      return <InputButton onPress={this.saveUser} text='Save' />
     }
   }
 
   render() {
     return (
-      <View style={styles.form}>
-        <Text style={styles.label}>ACCOUNT INFO</Text>
-        <FormInput placeholder='Emily'
+      <FormContainer>
+        <FormLabel label='ACCOUNT INFO' />
+
+        <InputContainer>
+          <FormInput placeholder='Emily'
+                     required={true}
+                     format='any'
+                     autoCapitalize='words'
+                     value={this.state.user.first_name}
+                     onChangeText={(first_name)=> this.setState({user: Object.assign({}, this.state.user, {first_name})})}
+                     label='First Name'
+                     returnKeyType='next'
+                     defaultValue={this.state.user.first_name} />
+
+         <InputSeparator />
+
+         <FormInput placeholder='Skywalker'
+                    required={true}
+                    format='any'
+                    autoCapitalize='words'
+                    value={this.state.user.last_name}
+                    onChangeText={(last_name)=> this.setState({user: Object.assign({}, this.state.user, {last_name})})}
+                    label='Last Name'
+                    returnKeyType='next'
+                    defaultValue={this.state.user.last_name} />
+
+        <InputSeparator />
+
+        <FormInput placeholder='emily@jedi.org'
                    required={true}
-                   format='any'
-                   autoCapitalize='words'
-                   value={this.state.user.first_name}
-                   onChangeText={(first_name)=> this.setState({user: Object.assign({}, this.state.user, {first_name})})}
-                   label='First Name'
+                   format='email'
+                   keyboardType='email-address'
+                   value={this.state.user.email}
+                   onChangeText={(email)=> this.setState({user: Object.assign({}, this.state.user, {email})})}
+                   label='Email'
                    returnKeyType='next'
-                   defaultValue={this.state.user.first_name} />
+                   defaultValue={this.state.user.email} />
 
-       <FormInput placeholder='Skywalker'
-                  required={true}
-                  format='any'
-                  autoCapitalize='words'
-                  value={this.state.user.last_name}
-                  onChangeText={(last_name)=> this.setState({user: Object.assign({}, this.state.user, {last_name})})}
-                  label='Last Name'
-                  returnKeyType='next'
-                  defaultValue={this.state.user.last_name} />
+         <InputSeparator />
 
-      <FormInput placeholder='emily@jedi.org'
-                 required={true}
-                 format='email'
-                 keyboardType='email-address'
-                 value={this.state.user.email}
-                 onChangeText={(email)=> this.setState({user: Object.assign({}, this.state.user, {email})})}
-                 label='Email'
-                 returnKeyType='next'
-                 defaultValue={this.state.user.email} />
-
-       <FormInput placeholder='Current Password'
-                  required={true}
-                  format='password'
-                  inputType='password'
-                  value={this.state.currentPassword}
-                  onChangeText={(currentPassword)=> this.setState({currentPassword})}
-                  label='Password'
-                  returnKeyType='done' />
-
+         <FormInput placeholder='Current Password'
+                    required={true}
+                    format='password'
+                    inputType='password'
+                    value={this.state.currentPassword}
+                    onChangeText={(currentPassword)=> this.setState({currentPassword})}
+                    label='Password'
+                    returnKeyType='done' />
+        </InputContainer>
         {this.saveButton()}
-      </View>
+      </FormContainer>
     )
   }
 }
