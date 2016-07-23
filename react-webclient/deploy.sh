a14072a007288b89e6fcaa80ced9ff793b7f24be
Dillon Hafer <dh@dillonhafer.com>
2016-07-22 22:12:34 -0500

    Update front-end deploy script to include webpack var

--- react-webclient/deploy.sh
+++ react-webclient/deploy.sh
@@ -1,5 +1,5 @@
 source .env
-API_URL=$API_URL npm run build
+API_URL=$API_URL WEBPACK_HOSTNAME=$WEBPACK_HOSTNAME npm run build
 aws s3 sync static s3://$S3_BUCKET_NAME
 aws s3 sync build s3://$S3_BUCKET_NAME
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html
