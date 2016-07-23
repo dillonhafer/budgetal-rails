5991dbc52a00d639278bb0224c8f7c6e0c4f1f53
Dillon Hafer <dh@dillonhafer.com>
2016-07-22 19:59:52 -0500

    Patches no longer needed

--- ios-client/patches/patch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This applies a patch to fix RN 27. Delete when trouble is over
-cp patches/RN-27.patch node_modules/react-native/Libraries/CustomComponents/NavigationExperimental/NavigationCard.js
-cp patches/RN-27a.patch node_modules/react-native/Libraries/Experimental/SwipeableRow/SwipeableRow.js
