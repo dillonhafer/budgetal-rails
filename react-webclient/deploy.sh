2fdeae77a050c903ea3a1d61d87b2ca8d23a09cf
Dillon Hafer <dh@dillonhafer.com>
2016-09-12 10:59:12 -0500

    Fix don't set cache-control header on index.html

--- react-webclient/deploy.sh
+++ react-webclient/deploy.sh
@@ -2,5 +2,6 @@ set -e
 source .env
 API_URL=$API_URL WEBPACK_HOSTNAME=$WEBPACK_HOSTNAME npm run build
 aws s3 sync static s3://$S3_BUCKET_NAME --cache-control max-age=315360000
-aws s3 sync build s3://$S3_BUCKET_NAME --cache-control max-age=315360000
+aws s3 sync build s3://$S3_BUCKET_NAME --exclude "*.html" --cache-control max-age=315360000
+aws s3 sync build s3://$S3_BUCKET_NAME --exclude "*" --include "index.html"
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html
