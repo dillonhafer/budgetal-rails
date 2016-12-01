965d6fa1d30d64b201b36ae1a57cccc73f1462e2
Dillon Hafer <dh@dillonhafer.com>
2016-12-01 09:21:04 -0600

    Support HMR with styles

--- react-webclient/webpack.config.js
+++ react-webclient/webpack.config.js
@@ -23,8 +23,8 @@ module.exports = {
         loaders: ['react-hot', 'babel-loader?'+JSON.stringify({presets:['react']})],
       },
       { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.(sass|scss)$/, loader: "style-loader!css-loader!sass-loader" },
       { test: /\.less$/, loader: 'style-loader!css-loader!less-loader?'+theme},
-      { test: /\.(scss|sass)$/, loader: ExtractTextPlugin.extract('style-loader', "css!sass!") },
       { test: /\.(png|gif|otf|eot|svg|ttf|woff)/, loader: 'url-loader?limit=100000' },
       { test: /\.jpg$/, loader: "file-loader" }
     ]
