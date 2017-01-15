1cbd6c28491a2124eff1cdbbc1690c7bcb83101e
Dillon Hafer <dh@dillonhafer.com>
2017-01-15 11:25:47 -0600

    Update site styles

--- react-webclient/src/assets/stylesheets/theme.js
+++ react-webclient/src/assets/stylesheets/theme.js
@@ -1,6 +1,10 @@
 module.exports = JSON.stringify({
   modifyVars: {
-    'primary-color': '#6699FF',
-    'link-color': '#6699FF',
+    // Color
+    'icon-url': '/fonts/iconfont',
+    'primary-color': '#1a98fc',
+    // 'success-color': '#8DAA37',
+    // 'error-color': '#D75351',
+    'btn-padding-lg': '9px 20px 10px 20px',
   }
 });
