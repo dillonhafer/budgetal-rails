523b7f53f69877c4fa56c840871e7dc1e1202d69
Dillon Hafer <dh@dillonhafer.com>
2017-02-27 23:03:53 -0600

    Fix color scheme from antd update

--- react-webclient/src/assets/stylesheets/theme.js
+++ react-webclient/src/assets/stylesheets/theme.js
@@ -1,10 +1,9 @@
 module.exports = JSON.stringify({
   modifyVars: {
-    // Color
     'icon-url': '/fonts/iconfont',
     'primary-color': '#1a98fc',
-    // 'success-color': '#8DAA37',
-    // 'error-color': '#D75351',
+    'success-color': '#87d068',
+    'error-color': '#f50',
     'btn-padding-lg': '9px 20px 10px 20px',
   }
 });
