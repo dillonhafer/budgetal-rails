10d98586d37720bd5ff6ca156b8a922f5a47c546
Dillon Hafer <dh@dillonhafer.com>
2017-02-28 20:27:57 -0600

    Update frontend deploy script

--- react-webclient/deploy.sh
+++ react-webclient/deploy.sh
@@ -1,8 +1,5 @@
 set -e
 source .env
 API_URL=$API_URL WEBPACK_HOSTNAME=$WEBPACK_HOSTNAME yarn run build
-aws s3 sync static s3://$S3_BUCKET_NAME --cache-control max-age=315360000
-aws s3 sync build s3://$S3_BUCKET_NAME --exclude "*.html" --cache-control max-age=315360000
-aws s3 sync build s3://$S3_BUCKET_NAME --exclude "*" --include "index.html"
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html
+rsync -avzh --include='static/' --include='build/' $RSYNC_REMOTE_PARAM
 rm -rf build
