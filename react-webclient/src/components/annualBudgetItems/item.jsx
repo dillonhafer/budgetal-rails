532404d906e981ce78fbb000994431fba24dff9d
Dillon Hafer <dh@dillonhafer.com>
2017-09-02 10:54:18 -0500

    Fix form validation on annual budget items

--- react-webclient/src/components/annualBudgetItems/item.jsx
+++ react-webclient/src/components/annualBudgetItems/item.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {numberToCurrency} from '../../utils/helpers';
+import { numberToCurrency } from '../../utils/helpers';
 import moment from 'moment';
-import {round,times} from 'lodash';
+import { round, times } from 'lodash';
 
 import {
   Badge,
@@ -21,55 +21,73 @@ export default class AnnualBudgetItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showProgress: false
-    }
+      showProgress: false,
+    };
   }
 
   static propTypes = {
-    budgetItem: React.PropTypes.object.isRequired
+    budgetItem: React.PropTypes.object.isRequired,
   };
 
   editItem = () => {
     this.props.handleOnCardClick(this.props.budgetItem);
-  }
+  };
 
   deleteItem = () => {
     this.props.handleOnDeleteClick(this.props.budgetItem);
-  }
+  };
 
   showProgress = () => {
-    this.setState({showProgress: true});
-  }
+    this.setState({ showProgress: true });
+  };
 
   hideProgress = () => {
-    this.setState({showProgress: false});
-  }
+    this.setState({ showProgress: false });
+  };
 
   getProgressModal(item, visible) {
-    const startDate = moment(item.due_date).subtract((item.payment_intervals+1), "months");
-    const month   = round((item.amount / item.payment_intervals));
-    const title   = `Accumulation Progress for ${item.name}`;
-    const dataSource = times(item.payment_intervals, (key) => {
-      const date = startDate.add(1, "months").format("LL");
-      const badgeStatus = moment().diff(startDate) > 0 ? "success" : "error";
+    const startDate = moment(item.due_date).subtract(
+      item.payment_intervals + 1,
+      'months'
+    );
+    const month = round(item.amount / item.payment_intervals);
+    const title = `Accumulation Progress for ${item.name}`;
+    const dataSource = times(item.payment_intervals, key => {
+      const date = startDate.add(1, 'months').format('LL');
+      const badgeStatus = moment().diff(startDate) > 0 ? 'success' : 'error';
       return {
         key: key,
-        date: <span><Badge status={badgeStatus} /> {date}</span>,
-        amount: numberToCurrency(month * (key+1))
-      }
+        date: (
+          <span>
+            <Badge status={badgeStatus} /> {date}
+          </span>
+        ),
+        amount: numberToCurrency(month * (key + 1)),
+      };
     });
 
     const columns = ['Date', 'Amount'].map(title => {
       return {
         title,
         key: title.toLowerCase(),
-        dataIndex: title.toLowerCase()
-      }
+        dataIndex: title.toLowerCase(),
+      };
     });
 
     return (
-      <Modal title={title} visible={visible} footer={null} onCancel={this.hideProgress}>
-        <Table dataSource={dataSource} pagination={false} size="small" columns={columns} bordered />
+      <Modal
+        title={title}
+        visible={visible}
+        footer={null}
+        onCancel={this.hideProgress}
+      >
+        <Table
+          dataSource={dataSource}
+          pagination={false}
+          size="small"
+          columns={columns}
+          bordered
+        />
       </Modal>
     );
   }
@@ -78,42 +96,56 @@ export default class AnnualBudgetItem extends React.Component {
     return (
       <Menu>
         <Menu.Item>
-          <a className="primary-color" onClick={this.showProgress}><Icon type="area-chart" /> Progress</a>
+          <a className="primary-color" onClick={this.showProgress}>
+            <Icon type="area-chart" /> Progress
+          </a>
         </Menu.Item>
         <Menu.Item>
-          <a className="primary-color" onClick={this.editItem}><Icon type="edit" /> Edit</a>
+          <a className="primary-color" onClick={this.editItem}>
+            <Icon type="edit" /> Edit
+          </a>
         </Menu.Item>
         <Menu.Item>
-          <a className="alert-color" onClick={this.deleteItem}><Icon type="delete" /> Delete</a>
+          <a className="alert-color" onClick={this.deleteItem}>
+            <Icon type="delete" /> Delete
+          </a>
         </Menu.Item>
       </Menu>
     );
   }
 
   render() {
-    const item    = this.props.budgetItem;
-    const name    = item.name;
+    const item = this.props.budgetItem;
+    const name = item.name;
     const loading = item.loading;
-    const amount  = numberToCurrency(item.amount);
-    const date    = moment(item.due_date).format('LL');
-    const month   = numberToCurrency(round((item.amount / item.payment_intervals)));
-    const color   = item.paid ? '#87d068' : '#cacaca';
-    const menu    = this.getMenu();
+    const amount = numberToCurrency(item.amount);
+    const date = moment(item.due_date).format('LL');
+    const month = numberToCurrency(round(item.amount / item.payment_intervals));
+    const color = item.paid ? '#87d068' : '#cacaca';
+    const menu = this.getMenu();
     return (
-      <Col className="card" xs={24} sm={12} md={8} lg={6}>
-        <Card loading={loading} title={name} extra={
+      <Col className="card" xs={24} sm={12} md={8} lg={8}>
+        <Card
+          loading={loading}
+          title={name}
+          extra={
             <div className="annual-item-crud">
               <Dropdown overlay={menu} trigger={['click']}>
                 <Button type="ghost" shape="circle" icon="ellipsis" />
               </Dropdown>
             </div>
-          }>
-          <div className='text-center'>
+          }
+        >
+          <div className="text-center">
             <p>
-              In order to reach <b>{amount}</b><br />
-              by <b>{date}</b><br />
+              In order to reach <b>{amount}</b>
+              <br />
+              by <b>{date}</b>
+              <br />
               you need to save
-              <br/><b>{month}/month</b><br />
+              <br />
+              <b>{month}/month</b>
+              <br />
             </p>
             <Tag color={color}>Paid</Tag>
           </div>
--- react-webclient/src/components/annualBudgetItems/item_list.jsx
+++ react-webclient/src/components/annualBudgetItems/item_list.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import AnnualBudgetItem from './item';
 import _ from 'lodash';
 
-import {
-  Badge,
-  Button,
-  Card,
-  Col,
-  Row,
-} from 'antd';
+import { Badge, Button, Card, Col, Row } from 'antd';
 
 export default class AnnualBudgetItemList extends React.Component {
   constructor(props) {
@@ -16,34 +10,46 @@ export default class AnnualBudgetItemList extends React.Component {
   }
 
   static propTypes = {
-    annualBudgetItems: React.PropTypes.array.isRequired
-  }
+    annualBudgetItems: React.PropTypes.array.isRequired,
+  };
 
   budgetItems(items) {
     if (_.isEmpty(items)) {
-      return <p className='text-center'>You haven't added any budget items yet.</p>;
+      return (
+        <p className="text-center">You haven't added any budget items yet.</p>
+      );
     } else {
       return this.buildList(items);
     }
   }
 
-  buildList = (budgetItems) => {
-    return (budgetItems.map((budgetItem, index) => {
+  buildList = budgetItems => {
+    return budgetItems.map((budgetItem, index) => {
       return (
-        <AnnualBudgetItem handleOnDeleteClick={this.props.handleOnDeleteClick}
-                          handleOnCardClick={this.props.handleOnCardClick}
-                          budgetItem={budgetItem}
-                          key={index} />
-      )
-    }))
-  }
+        <AnnualBudgetItem
+          handleOnDeleteClick={this.props.handleOnDeleteClick}
+          handleOnCardClick={this.props.handleOnCardClick}
+          budgetItem={budgetItem}
+          key={index}
+        />
+      );
+    });
+  };
 
   render() {
     return (
-      <Row className='card-grid'>
+      <Row className="card-grid">
         {this.budgetItems(this.props.annualBudgetItems)}
-        <Col className="card text-center" span="6">
-          <Button type="primary" icon="plus-circle" className="add-item-button" onClick={this.props.onClick} size="large">Add an Item</Button>
+        <Col className="card text-center" span={8}>
+          <Button
+            type="primary"
+            icon="plus-circle"
+            className="add-item-button"
+            onClick={this.props.onClick}
+            size="large"
+          >
+            Add an Item
+          </Button>
         </Col>
       </Row>
     );
