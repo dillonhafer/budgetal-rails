0b993ef15f6918ff61782f0f7001d46f81acafbc
Dillon Hafer <dh@dillonhafer.com>
2017-09-02 09:58:00 -0500

    Update error pages

--- react-webclient/src/components/layout/maintenance.jsx
+++ react-webclient/src/components/layout/maintenance.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 export default class Maintenance extends React.Component {
   render() {
+    const style = {
+      padding: '24px',
+      margin: '24px',
+      marginTop: '100px',
+    };
     return (
-      <div className='not-found'>
-        <div className='text-center'>
-          <h3 className='blue'>We are performing scheduled maintenance right now.</h3>
+      <div className="not-found" style={style}>
+        <div className="text-center">
+          <h1 className="primary-color">
+            We are performing scheduled maintenance right now.
+          </h1>
           <hr />
           <p>We will be done shortly.</p>
         </div>
       </div>
-    )
+    );
   }
 }
--- react-webclient/src/components/layout/not-found.jsx
+++ react-webclient/src/components/layout/not-found.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 export default class NotFound extends React.Component {
   render() {
+    const style = {
+      padding: '24px',
+      margin: '24px',
+      marginTop: '100px',
+    };
     return (
-      <div className='not-found'>
-        <div className='text-center'>
-          <h3 className='blue'>The page you were looking for doesn't exist.</h3>
+      <div className="not-found" style={style}>
+        <div className="text-center">
+          <h1 className="primary-color">
+            The page you were looking for doesn't exist.
+          </h1>
           <hr />
           <p>You may have mistyped the address or the page may have moved.</p>
         </div>
       </div>
-    )
+    );
   }
 }
